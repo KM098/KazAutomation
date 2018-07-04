4f64868f2ca6be6e7fc39c31ffd02e2056fa7279
Kazuyo Makimura <kazuyo.makimura@lowes.com>
2018-07-04 13:31:21 -0700

    Updated

---
+++ Pages/BasePageObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+using Pages.Interface;
+
+namespace Pages
+{
+    public class BasePageObjects : IBasePageObjects
+    {
+        public IWebDriver Driver { get; set; }
+        public string PageTitle { get; set; }
+        public string PageUrl { get; set; }
+        public WebDriverWait Wait { get; set; }
+    }
+}
--- Tests/HomePageTests.cs
+++ Tests/HomePageTests.cs
@@ -23,56 +23,60 @@ namespace Tests
         private IBasePageObjects _iBasePageObjects;
 
 
-         [OneTimeSetUp]
-         public void TestInitialize()
-            {
-                _driver = InitializeDriver(); ;
-                InitializePageMethods();
-            }
+        [SetUp]
+        public void TestInitialize()
+        {
+            _driver = InitializeDriver();
+            InitializePageMethods();
+        }
 
-          [Test]
-          public void OpneHomePage()
-          {
-            Homepage.GoTo("https://www.lowes.ca");
+        [Test]
+        public void OpneHomePage()
+        {
+            Homepage.GoTo("www.lowes.ca");
             Assert.IsTrue(Homepage.IsAt(), "Homepage doesn't open");
             Assert.IsTrue(Homepage.IsEmailPopUpDisplayed(), "Eamil pop up doesn't open");
-          }
+        }
 
-          [Test]
-          public void CloseEmailPopBox()
-          {
-              Homepage.GoTo("https://www.lowes.ca");
-              Homepage.CloseEmailPopUp();
-              Assert.IsFalse(Homepage.IsEmailPopUpDisplayed(), "Eamil pop up is not closed");
-          }
+        [Test]
+        public void CloseEmailPopBox()
+        {
+            Homepage.GoTo("https://www.lowes.ca");
+            Homepage.CloseEmailPopUp();
+            Assert.IsFalse(Homepage.IsEmailPopUpDisplayed(), "Eamil pop up is not closed");
+        }
 
-          [TearDown]
-          public void TestCleanup()
-          {
-              _driver.Quit();
-          }
+        [TearDown]
+        public void TestCleanup()
+        {
+            _driver.Quit();
+        }
 
-          public void InitializePageMethods()
-          {
-            //_iPopUp = new 
-            //_iBasePageObjects = new 
+        public void InitializePageMethods()
+        {
+            _iPopUp = new PopUp(_driver);
+            _iBasePageObjects = new BasePageObjects();
             _iBasePageObjects.Driver = _driver;
             _iBasePageObjects.PageTitle = "Lowe's Canada: Home Improvement, Appliances, Tools, Bathroom, Kitchen";
-            _iBasePageObjects.PageUrl = "https://www.lowes.ca";
+            _iBasePageObjects.PageUrl = "www.lowes.ca";
             _iBasePageObjects.Wait = new WebDriverWait(_driver, TimeSpan.FromMinutes(2));
             Homepage = new HomePage(_iPopUp, _iBasePageObjects);
-           }
+        }
 
-            public IWebDriver InitializeDriver()
-            {
-                var options = new ChromeOptions();
-                options.AddArgument("--no-sandbox");
-                options.AddArgument("--headless");
-                _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options, TimeSpan.FromSeconds(120));
+        public IWebDriver InitializeDriver()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--headless");
+            
+            using (_driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {                
                 _driver.Manage().Window.Maximize();
                 _driver.Manage().Cookies.DeleteAllCookies();
-                return _driver;
             }
+            
+            return _driver;
+        }
     }
 }
 
